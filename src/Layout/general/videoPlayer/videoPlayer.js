6fa905defd38153157a5fe5b2ba4840c5caeb69b
myrza256 <dev.myrzabek@gmail.com>
2021-03-09 20:04:42 +0200

    player

--- src/Layout/general/videoPlayer/videoPlayer.js
+++ src/Layout/general/videoPlayer/videoPlayer.js
@@ -2,32 +2,31 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import '../../landing/components/VideoPlayerModal/VideoPlayerModal.scoped.scss'
 import './videoPlayer.scss'
-import { Player, Ui, DefaultUi, Video } from '@vime/react';
+import { Player, Video, DefaultUi, Youtube } from '@vime/react';
 const VideoPlayer = ({ className, url }) => {
-
+console.log(url)
 
   return (
-    <ReactPlayer
-      className={['videoPlayer', className].join(' ')}
-      url={url || 'https://vimeo.com/259411563'}
-      controls={true}
-      width={'100%'}
-      height={'100%'}
-      playsinline={true}
-      config={{
-        vimeo: {
-          playsinline: true
-        }
-      }}
-    />
-//<Player>
-  //    {/* Provider component is placed here. */}
-   //   <Video>
-   //       <source data-src="https://vimeo.com/192834629" type="video/mp4" />
-   //     </Video>
-   //   <DefaultUi>
-  //    </DefaultUi>
-  //  </Player>
+    // <ReactPlayer
+    //   className={['videoPlayer', className].join(' ')}
+    //   url={url || 'https://vimeo.com/259411563'}
+    //   controls={true}
+    //   width={'100%'}
+    //   height={'100%'}
+    //   playsinline={true}
+    //   config={{
+    //     vimeo: {
+    //       playsinline: true
+    //     }
+    //   }}
+    // />
+  <Player
+    theme="dark"
+    style={{ '--vm-player-theme': '#e86c8b' }}
+  >
+    <Youtube videoId={`${url ? url.split('https://youtu.be/')[1] : "DyTCOwB0DVw"}`} />
+    <DefaultUi />
+  </Player>
   )
 }
 
